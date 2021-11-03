0f5ff1164a01151dfb5e6206cd49d299ceed556e
Viegas <mvrdgs@gmail.com>
2021-11-03 18:33:21 -0300

    feat: criado estrutura basica do formulario

--- src/Views/Calculator.jsx
+++ src/Views/Calculator.jsx
@@ -3,7 +3,23 @@ import React from 'react';
 function Calculator() {
   return (
     <div>
-      Teste
+      <label htmlFor="width">
+        Largura:
+        <input id="width" type="text" />
+      </label>
+      <label htmlFor="height">
+        Altura:
+        <input id="height" type="text" />
+      </label>
+      <label htmlFor="windows">
+        Quantidade de janelas:
+        <input id="windows" type="text" />
+      </label>
+      <label htmlFor="doors">
+        Quantidade de portas:
+        <input id="doors" type="text" />
+      </label>
+      <button type="button">Calcular</button>
     </div>
   );
 }
