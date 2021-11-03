ed7d29c8bdd976c0f939f32c41a425edcb69891d
Viegas <mvrdgs@gmail.com>
2021-11-03 18:43:22 -0300

    feat: criado gerenciamento de estado

--- src/Views/Calculator.jsx
+++ src/Views/Calculator.jsx
@@ -1,23 +1,56 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+
+const wallParameters = {
+  width: '0',
+  height: '0',
+  doors: '0',
+  windows: '0',
+};
 
 function Calculator() {
+  const [wallData, setWallData] = useState(wallParameters);
+
+  useEffect(() => { console.log(wallData); }, [wallData]);
+
+  const handleChange = ({ target }) => {
+    const { id, value } = target;
+
+    setWallData({ ...wallData, [id]: value });
+  };
+
   return (
     <div>
       <label htmlFor="width">
         Largura:
-        <input id="width" type="text" />
+        <input
+          id="width"
+          type="text"
+          onChange={handleChange}
+        />
       </label>
       <label htmlFor="height">
         Altura:
-        <input id="height" type="text" />
+        <input
+          id="height"
+          type="text"
+          onChange={handleChange}
+        />
       </label>
       <label htmlFor="windows">
         Quantidade de janelas:
-        <input id="windows" type="text" />
+        <input
+          id="windows"
+          type="text"
+          onChange={handleChange}
+        />
       </label>
       <label htmlFor="doors">
         Quantidade de portas:
-        <input id="doors" type="text" />
+        <input
+          id="doors"
+          type="text"
+          onChange={handleChange}
+        />
       </label>
       <button type="button">Calcular</button>
     </div>
